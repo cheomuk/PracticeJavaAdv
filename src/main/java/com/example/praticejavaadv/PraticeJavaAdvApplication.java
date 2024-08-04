5fb287e76d83407861b367b08701c10c00e68308
cheomuk <pkw9815@naver.com>
2024-08-04 16:19:44 +0900

    First Commit

---
+++ src/main/java/com/example/praticejavaadv/PraticeJavaAdvApplication.java
@@ -0,0 +1,13 @@
+package com.example.praticejavaadv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticeJavaAdvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticeJavaAdvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/praticejavaadv/PraticeJavaAdvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.praticejavaadv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticeJavaAdvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
