21f376bbc1a716f6c86eb04b60a23487bdf49398
cheomuk <pkw9815@naver.com>
2024-08-11 00:17:10 +0900

    Commit 240811

--- src/main/java/com/example/praticejavaadv/interrupt/ThreadStopMainV4.java
+++ src/main/java/com/example/praticejavaadv/interrupt/ThreadStopMainV4.java
@@ -3,7 +3,7 @@ package com.example.praticejavaadv.interrupt;
 import static com.example.praticejavaadv.util.MyLogger.log;
 import static com.example.praticejavaadv.util.ThreadUtils.sleep;
 
-public class ThreadStopMainV3 {
+public class ThreadStopMainV4 {
 
     public static void main(String[] args) {
         MyTask myTask = new MyTask();
@@ -21,17 +21,17 @@ public class ThreadStopMainV3 {
         @Override
         public void run() {
 
-            while (!Thread.currentThread().isInterrupted()) {   // 인터럽트 상태 변경 x
+            while (!Thread.interrupted()) {   // 인터럽트 상태 변경 o
                 log("작업 중");
             }
 
-            log("work 스레드 인터럽트 상태2 = " + Thread.currentThread().isInterrupted());   // true 출력
+            log("work 스레드 인터럽트 상태2 = " + Thread.currentThread().isInterrupted());   // false 출력
 
             try {
                 log("자원 정리");
-                Thread.sleep(1000); // 위 인터럽트의 상태가 변경되지 않았기 때문에 바로 Exception 이 발생한다.
+                Thread.sleep(1000); // 전과 반대로 상태가 변경되었기 때문에 Exception 이 발생하지 않는다.
                 log("자원 종료");
-            } catch (InterruptedException e) {  // 여기서 false 로 변경된다.
+            } catch (InterruptedException e) {
                 log("자원 정리 실패 - 자원 정리 중 인터럽트 발생");
                 log("work 스레드 인터럽트 상태3 = " + Thread.currentThread().isInterrupted());
             }
