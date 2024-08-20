44f59f6fe105a39128d42473938641b110e6d8bf
cheomuk <pkw9815@naver.com>
2024-08-20 23:59:10 +0900

    Commit 240820

---
+++ src/main/java/com/example/praticejavaadv/sync/BankAccountV4.java
@@ -0,0 +1,55 @@
+package com.example.praticejavaadv.sync;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static com.example.praticejavaadv.util.MyLogger.log;
+import static com.example.praticejavaadv.util.ThreadUtils.sleep;
+
+public class BankAccountV4 implements BankAccount {
+
+    private int balance;
+
+    private final Lock lock = new ReentrantLock();
+
+    public BankAccountV4(int initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    @Override
+    public boolean withdraw(int amount) {
+        log("거래 시작" + getClass().getSimpleName());
+
+        lock.lock();    // ReentrantLock 을 이용하여 Lock 을 걸기
+
+        try {
+            log("[검증 시작]");
+            if (balance < amount) {
+                log("검증 실패 출금액 : " + amount + ", 잔액 : " + balance);
+                return false;
+            }
+
+            // 잔고가 출금액보다 많으면, 진행
+            log("[검증 성공] 출금액 : " + amount + ", 잔액 : " + balance);
+            sleep(1000);    // 출금에 걸리는 시간
+            balance -= amount;
+
+            log("[출금 완료] 출금액 : " + amount + ", 잔액 : " + balance);
+        } finally { // unlock 은 lock 을 반환하기 위해 무조건 finally 에서 실행해야 한다.
+            lock.unlock();  // ReentrantLock 을 이용하여 Lock 해제
+        }
+
+        log("거래 종료");
+        return true;
+    }
+
+    @Override
+    public int getBalance() {
+        lock.lock();
+        try {
+            return balance;
+        } finally {
+            lock.unlock();
+        }
+    }
+}
---
+++ src/main/java/com/example/praticejavaadv/sync/BankAccountV6.java
@@ -0,0 +1,61 @@
+package com.example.praticejavaadv.sync;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static com.example.praticejavaadv.util.MyLogger.log;
+import static com.example.praticejavaadv.util.ThreadUtils.sleep;
+
+public class BankAccountV6 implements BankAccount {
+
+    private int balance;
+
+    private final Lock lock = new ReentrantLock();
+
+    public BankAccountV6(int initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    @Override
+    public boolean withdraw(int amount) {
+        log("거래 시작" + getClass().getSimpleName());
+
+        try {
+            if (!lock.tryLock(500, TimeUnit.MILLISECONDS)) {
+                log("[진입 실패] 이미 처리 중인 작업이 있습니다.");
+                return false;
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            log("[검증 시작]");
+            if (balance < amount) {
+                log("검증 실패 출금액 : " + amount + ", 잔액 : " + balance);
+                return false;
+            }
+
+            // 잔고가 출금액보다 많으면, 진행
+            log("[검증 성공] 출금액 : " + amount + ", 잔액 : " + balance);
+            sleep(1000);    // 출금에 걸리는 시간
+            balance -= amount;
+
+            log("[출금 완료] 출금액 : " + amount + ", 잔액 : " + balance);
+        } finally { // unlock 은 lock 을 반환하기 위해 무조건 finally 에서 실행해야 한다.
+            lock.unlock();  // ReentrantLock 을 이용하여 Lock 해제
+        }
+
+        log("거래 종료");
+        return true;
+    }
+
+    @Override
+    public int getBalance() {
+        lock.lock();
+        try {
+            return balance;
+        } finally {
+            lock.unlock();
+        }
+    }
+}
